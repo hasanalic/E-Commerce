5c8a04488ea3025d91e93a963aa350f5666eb24a
hasanalic <hasanacaliskan@gmail.com>
2024-07-22 20:12:17 +0300

    Added res

--- app/src/main/java/com/hasanalic/ecommerce/ui/MainActivity.kt
+++ app/src/main/java/com/hasanalic/ecommerce/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.hasanalic.ecommerce
+package com.hasanalic.ecommerce.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hasanalic.ecommerce.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
