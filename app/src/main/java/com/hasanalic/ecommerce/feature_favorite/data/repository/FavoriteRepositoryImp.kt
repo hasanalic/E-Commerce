f634d0cbf2ddd7fee4a583057573f5cfdaa3d674
hasanalic <hasanacaliskan@gmail.com>
2024-07-31 16:19:12 +0300

    Updated FavoriteRepository and added FavoriteRepositoryImp

---
+++ app/src/main/java/com/hasanalic/ecommerce/feature_favorite/data/repository/FavoriteRepositoryImp.kt
@@ -0,0 +1,66 @@
+package com.hasanalic.ecommerce.feature_favorite.data.repository
+
+import com.hasanalic.ecommerce.core.domain.model.DataError
+import com.hasanalic.ecommerce.core.domain.model.Result
+import com.hasanalic.ecommerce.feature_favorite.data.entity.FavoritesEntity
+import com.hasanalic.ecommerce.feature_favorite.data.local.FavoritesDao
+import com.hasanalic.ecommerce.feature_favorite.domain.repository.FavoriteRepository
+import javax.inject.Inject
+
+class FavoriteRepositoryImp @Inject constructor(
+    private val favoritesDao: FavoritesDao
+) : FavoriteRepository {
+    override suspend fun getFavoriteListByUserId(userId: String): Result<List<FavoritesEntity>, DataError.Local> {
+        return try {
+            val favoriteList = favoritesDao.getFavorites(userId)
+            favoriteList?.let {
+                Result.Success(it)
+            }?: Result.Error(DataError.Local.NOT_FOUND)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.UNKNOWN)
+        }
+    }
+
+    override suspend fun getFavoriteIdByUserIdAndProductId(
+        userId: String,
+        productId: String
+    ): Result<Int, DataError.Local> {
+        return try {
+            val favoriteId = favoritesDao.getFavoriteByProductId(userId, productId)
+            favoriteId?.let {
+                Result.Success(favoriteId)
+            }?: Result.Error(DataError.Local.NOT_FOUND)
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.UNKNOWN)
+        }
+    }
+
+    override suspend fun insertFavoriteAndGetId(favoritesEntity: FavoritesEntity): Result<Long, DataError.Local> {
+        return try {
+            val favoriteId = favoritesDao.insertFavorite(favoritesEntity)
+            if (favoriteId > 0) {
+                Result.Success(favoriteId)
+            } else {
+                Result.Error(DataError.Local.INSERTION_FAILD)
+            }
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.UNKNOWN)
+        }
+    }
+
+    override suspend fun deleteFavorite(
+        userId: String,
+        productId: String
+    ): Result<Unit, DataError.Local> {
+        return try {
+            val affectedRowCount = favoritesDao.deleteFavorite(userId, productId)
+            if (affectedRowCount > 0) {
+                Result.Success(Unit)
+            } else {
+                Result.Error(DataError.Local.DELETION_FAILED)
+            }
+        } catch (e: Exception) {
+            Result.Error(DataError.Local.UNKNOWN)
+        }
+    }
+}
--- app/src/main/java/com/hasanalic/ecommerce/feature_favorite/domain/repository/FavoriteRepository.kt
+++ app/src/main/java/com/hasanalic/ecommerce/feature_favorite/domain/repository/FavoriteRepository.kt
@@ -1,4 +1,16 @@
 package com.hasanalic.ecommerce.feature_favorite.domain.repository
 
+import com.hasanalic.ecommerce.core.domain.model.DataError
+import com.hasanalic.ecommerce.core.domain.model.Result
+import com.hasanalic.ecommerce.feature_favorite.data.entity.FavoritesEntity
+
 interface FavoriteRepository {
+
+    suspend fun getFavoriteListByUserId(userId: String): Result<List<FavoritesEntity>, DataError.Local>
+
+    suspend fun getFavoriteIdByUserIdAndProductId(userId: String, productId: String): Result<Int, DataError.Local>
+
+    suspend fun insertFavoriteAndGetId(favoritesEntity: FavoritesEntity): Result<Long, DataError.Local>
+
+    suspend fun deleteFavorite(userId: String, productId: String): Result<Unit, DataError.Local>
 }
