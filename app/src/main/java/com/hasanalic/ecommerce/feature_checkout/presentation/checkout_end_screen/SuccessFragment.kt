5b470f75ef488a1d95e638cdd612a4eb76f770ae
hasanalic <hasanacaliskan@gmail.com>
2024-08-26 14:36:30 +0300

    Updated SuccessFragment

--- app/src/main/java/com/hasanalic/ecommerce/feature_checkout/presentation/checkout_end_screen/SuccessFragment.kt
+++ app/src/main/java/com/hasanalic/ecommerce/feature_checkout/presentation/checkout_end_screen/SuccessFragment.kt
@@ -1,5 +1,6 @@
 package com.hasanalic.ecommerce.feature_checkout.presentation.checkout_end_screen
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import com.hasanalic.ecommerce.databinding.FragmentSuccessBinding
 import com.hasanalic.ecommerce.feature_home.domain.repository.HomeRepository
+import com.hasanalic.ecommerce.feature_orders.presentation.OrderActivity
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -19,8 +21,6 @@ class SuccessFragment: Fragment() {
 
     private var _binding: FragmentSuccessBinding? = null
     private val binding get() = _binding!!
-
-    //private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,24 +40,12 @@ class SuccessFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*
-        auth = FirebaseAuth.getInstance()
-        val currentUser = auth.currentUser
-        if (currentUser == null) {
-            binding.buttonPreviewOrder.text = "Ana Sayfaya Dön"
-            binding.buttonPreviewOrder.setOnClickListener {
-                requireActivity().finish()
-            }
-        } else {
-            binding.buttonPreviewOrder.text = "Siparişleri Görüntüle"
-            binding.buttonPreviewOrder.setOnClickListener {
-                val intent = Intent(requireActivity(),OrderActivity::class.java)
-                startActivity(intent)
-                requireActivity().finish()
-            }
+        binding.buttonPreviewOrder.text = "Siparişleri Görüntüle"
+        binding.buttonPreviewOrder.setOnClickListener {
+            val intent = Intent(requireActivity(), OrderActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
         }
-
-         */
     }
 
     override fun onDestroyView() {
