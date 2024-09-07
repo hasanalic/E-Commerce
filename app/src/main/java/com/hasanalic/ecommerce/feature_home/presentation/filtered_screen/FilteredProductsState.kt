f7963220aad181af56c3019710f7e978c1121c40
hasanalic <hasanacaliskan@gmail.com>
2024-09-07 16:30:31 +0300

    Updated FilteredProductsViewModel

--- app/src/main/java/com/hasanalic/ecommerce/feature_home/presentation/filtered_screen/FilteredProductsState.kt
+++ app/src/main/java/com/hasanalic/ecommerce/feature_home/presentation/filtered_screen/FilteredProductsState.kt
@@ -6,7 +6,7 @@ import com.hasanalic.ecommerce.feature_home.domain.model.Product
 data class FilteredProductsState (
     val isLoading: Boolean = false,
     val userId: String = ANOMIM_USER_ID,
-    val productList: MutableList<Product> = mutableListOf(),
+    val productList: List<Product> = listOf(),
     val shouldUserMoveToAuthActivity: Boolean = false,
     val dataError: String? = null,
     val actionError: String? = null
--- app/src/main/java/com/hasanalic/ecommerce/feature_home/presentation/filtered_screen/FilteredProductsViewModel.kt
+++ app/src/main/java/com/hasanalic/ecommerce/feature_home/presentation/filtered_screen/FilteredProductsViewModel.kt
@@ -11,16 +11,15 @@ import com.hasanalic.ecommerce.core.presentation.utils.UserConstants.ANOMIM_USER
 import com.hasanalic.ecommerce.feature_home.data.local.entity.FavoritesEntity
 import com.hasanalic.ecommerce.feature_home.data.local.entity.ShoppingCartItemsEntity
 import com.hasanalic.ecommerce.feature_home.domain.use_case.favorite_use_cases.FavoriteUseCases
-import com.hasanalic.ecommerce.feature_home.domain.use_case.home_use_cases.HomeUseCases
+import com.hasanalic.ecommerce.feature_home.domain.use_case.filtered_products_use_cases.FilteredProductsUseCases
 import com.hasanalic.ecommerce.feature_home.domain.use_case.shopping_cart_use_cases.ShoppingCartUseCases
-import com.hasanalic.ecommerce.feature_home.presentation.home_screen.HomeState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FilteredProductsViewModel @Inject constructor(
-    private val homeUseCases: HomeUseCases,
+    private val filteredProductsUseCases: FilteredProductsUseCases,
     private val shoppingCartUseCases: ShoppingCartUseCases,
     private val favoriteUseCases: FavoriteUseCases,
     private val sharedPreferencesUseCases: SharedPreferencesUseCases
@@ -29,213 +28,242 @@ class FilteredProductsViewModel @Inject constructor(
     private var _filteredProductsState = MutableLiveData(FilteredProductsState())
     val filteredProducsState: LiveData<FilteredProductsState> = _filteredProductsState
 
+    fun checkUserId() {
+        val userId = sharedPreferencesUseCases.getUserIdUseCase()
+        userId?.let {
+            _filteredProductsState.value = FilteredProductsState(userId = it)
+        }
+    }
 
-    fun getProductsBySearchKey(searchKey: String) {
+    fun getProductsByKeyword(keyword: String) {
         _filteredProductsState.value = _filteredProductsState.value!!.copy(isLoading = true)
         viewModelScope.launch {
-            /*
-            val userId = _homeState.value!!.userId
-
-            when(val result = homeUseCases.getProductsByUserIdUseCase(userId)) {
-                is Result.Error -> handleGetProductsError(result.error)
+            val userId = _filteredProductsState.value!!.userId
+            when(val result = filteredProductsUseCases.getProductsByKeywordUseCase(userId, keyword)) {
+                is Result.Error -> handleGetProductsByKeywordError(result.error)
                 is Result.Success -> {
-                    _homeState.value = homeState.value!!.copy(
+                    _filteredProductsState.value = _filteredProductsState.value!!.copy(
                         isLoading = false,
-                        productList = result.data.toMutableList()
+                        productList = result.data
                     )
                 }
             }
-
-             */
         }
     }
 
-    private fun handleGetProductsBySearchKeyError(error: DataError.Local) {
-        /*
-        when(error) {
-            DataError.Local.NOT_FOUND -> {
-                _homeState.value = HomeState(
-                    dataError = "Ürünler getirilemedi."
-                )
-            }
-            DataError.Local.UNKNOWN -> {
-                _homeState.value = HomeState(
-                    dataError = "Bilinmeyen bir hata nedeniyle ürünler getirilemedi."
-                )
-            }
-            else -> {}
+    private fun handleGetProductsByKeywordError(error: DataError.Local) {
+        val errorMessage = when(error) {
+            DataError.Local.NOT_FOUND -> "Ürünler getirilemedi."
+            DataError.Local.UNKNOWN -> "Bilinmeyen bir hata nedeniyle ürünler getirilemedi."
+            else -> null
         }
 
-         */
+        _filteredProductsState.value = FilteredProductsState(
+            dataError = errorMessage,
+            isLoading = false
+        )
     }
 
-    fun removeProductFromCart(productId: String, itemIndex: Int) {
-        /*
+
+    fun checkIfProductAlreadyInCart(productId: String, itemIndex: Int) {
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(isLoading = true)
         viewModelScope.launch {
-            val userId = _homeState.value!!.userId
-
-            when(val result = shoppingCartUseCases.deleteShoppingCartItemEntityUseCase(userId, productId)) {
-                is Result.Error -> handleRemoveProductFromCartError(result.error)
-                is Result.Success -> { setAddedToCart(itemIndex, false) }
+            val userId = _filteredProductsState.value!!.userId
+            when(val result = shoppingCartUseCases.checkIfProductInCartUseCase(userId, productId)) {
+                is Result.Error -> handleCheckProductError(result.error)
+                is Result.Success -> {
+                    if (result.data) {
+                        setAddedToCart(itemIndex, true)
+                    } else {
+                        addProductToCart(userId, productId, itemIndex)
+                    }
+                }
             }
         }
-
-         */
     }
 
-    private fun handleRemoveProductFromCartError(error: DataError.Local) {
-        /*
-        when(error) {
-            DataError.Local.DELETION_FAILED -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Ürün, alışveriş sepetinden silinemedi."
-                )
-            }
-            DataError.Local.UNKNOWN -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Bilinmeyen bir hata meydana geldi."
-                )
-            }
-            else -> {}
+    private suspend fun addProductToCart(userId: String, productId: String, itemIndex: Int) {
+        val shoppingCartEntity = ShoppingCartItemsEntity(userId, productId, 1)
+        when (val result = shoppingCartUseCases.insertShoppingCartItemEntityUseCase(shoppingCartEntity)) {
+            is Result.Error -> handleAddProductToCartError(result.error)
+            is Result.Success -> setAddedToCart(itemIndex, true)
         }
-
-         */
-    }
-
-    fun addProductToCart(productId: String, itemIndex: Int) {
-        /*
-        viewModelScope.launch {
-            val userId = _homeState.value!!.userId
-            val shoppingCartEntity = ShoppingCartItemsEntity(userId, productId, 1)
-
-            when (val result = shoppingCartUseCases.insertShoppingCartItemEntityUseCase(shoppingCartEntity)) {
-                is Result.Error -> handleAddProductToCartError(result.error)
-                is Result.Success -> setAddedToCart(itemIndex, true)
-            }
-        }
-
-         */
     }
 
     private fun handleAddProductToCartError(error: DataError.Local) {
-        /*
-        when(error) {
-            DataError.Local.INSERTION_FAILED -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Ürün, alışveriş sepetine eklenemedi."
-                )
+        val message = when(error) {
+            DataError.Local.INSERTION_FAILED -> "Ürün, alışveriş sepetine eklenemedi."
+            DataError.Local.UNKNOWN -> "Bilinmeyen bir hata meydana geldi."
+            else -> null
+        }
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            actionError = message,
+            isLoading = false
+        )
+    }
+
+    fun checkIfProductNotInCart(productId: String, itemIndex: Int) {
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(isLoading = true)
+        viewModelScope.launch {
+            val userId = _filteredProductsState.value!!.userId
+            when(val result = shoppingCartUseCases.checkIfProductInCartUseCase(userId, productId)) {
+                is Result.Error -> handleCheckProductError(result.error)
+                is Result.Success -> {
+                    if (result.data) {
+                        removeProductFromCart(userId, productId, itemIndex)
+                    } else {
+                        setAddedToCart(itemIndex, false)
+                    }
+                }
             }
-            DataError.Local.UNKNOWN -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Bilinmeyen bir hata meydana geldi."
-                )
-            }
-            else -> {}
+        }
+    }
+
+    private suspend fun removeProductFromCart(userId: String, productId: String, itemIndex: Int) {
+        when(val result = shoppingCartUseCases.deleteShoppingCartItemEntityUseCase(userId, productId)) {
+            is Result.Error -> handleRemoveProductFromCartError(result.error)
+            is Result.Success -> { setAddedToCart(itemIndex, false) }
+        }
+    }
+
+    private fun handleRemoveProductFromCartError(error: DataError.Local) {
+        val message = when(error) {
+            DataError.Local.DELETION_FAILED -> "Ürün, alışveriş sepetinden silinemedi."
+            DataError.Local.UNKNOWN -> "Bilinmeyen bir hata meydana geldi."
+            else -> null
         }
 
-         */
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            actionError = message,
+            isLoading = false
+        )
     }
 
     private fun setAddedToCart(itemIndex: Int, isAddedToCart: Boolean) {
-        /*
-        val currentProductList = _homeState.value!!.productList
+        val currentProductList = _filteredProductsState.value!!.productList
         currentProductList[itemIndex].addedToShoppingCart = isAddedToCart
-        _homeState.value = _homeState.value!!.copy(
-            productList = currentProductList
+
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            productList = currentProductList,
+            isLoading = false
         )
-
-         */
     }
 
-    fun removeProductFromFavoritesIfUserAuthenticated(productId: String, itemIndex: Int) {
-        /*
-        val userId = _homeState.value!!.userId
-
-        if (userId == ANOMIM_USER_ID) {
-            _homeState.value = _homeState.value!!.copy(
-                shouldUserMoveToAuthActivity = true
-            )
-            return
-        }
-
-        viewModelScope.launch {
-            when(val result = favoriteUseCases.deleteFavoriteUseCase(userId, productId)) {
-                is Result.Error -> handleRemoveProductFromFavoritesError(result.error)
-                is Result.Success -> { setAddedToFavorites(itemIndex, false) }
-            }
-        }
-
-         */
-    }
-
-    private fun handleRemoveProductFromFavoritesError(error: DataError.Local) {
-        /*
-        when(error) {
-            DataError.Local.DELETION_FAILED -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Ürün, favorilerden kaldırılamadı."
-                )
-            }
-            DataError.Local.UNKNOWN -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Bilinmeyen bir hata meydana geldi."
-                )
-            }
-            else -> {}
-        }
-
-         */
-    }
 
     fun addProductToFavoritesIfUserAuthenticated(productId: String, itemIndex: Int) {
-        /*
-        val userId = _homeState.value!!.userId
+        val userId = _filteredProductsState.value!!.userId
 
         if (userId == ANOMIM_USER_ID) {
-            _homeState.value = _homeState.value!!.copy(
+            _filteredProductsState.value = _filteredProductsState.value!!.copy(
                 shouldUserMoveToAuthActivity = true
             )
             return
         }
 
         viewModelScope.launch {
-            val favoriteEntity = FavoritesEntity(userId, productId)
-            when(val result = favoriteUseCases.insertFavoriteAndGetIdUseCase(favoriteEntity)) {
-                is Result.Error -> handleAddProductToFavoritesError(result.error)
-                is Result.Success -> { setAddedToFavorites(itemIndex, true) }
+            checkIfProductAlreadyInFavorites(userId, productId, itemIndex)
+        }
+    }
+
+    private suspend fun checkIfProductAlreadyInFavorites(userId: String, productId: String, itemIndex: Int) {
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(isLoading = true)
+        when(val result = favoriteUseCases.checkIfProductInFavoritesUseCase(userId, productId)) {
+            is Result.Error -> handleCheckProductError(result.error)
+            is Result.Success -> {
+                if (result.data) {
+                    setAddedToFavorites(itemIndex, true)
+                } else {
+                    addProductToFavorites(userId, productId, itemIndex)
+                }
             }
         }
+    }
 
-         */
+    private suspend fun addProductToFavorites(userId: String, productId: String, itemIndex: Int) {
+        val favoriteEntity = FavoritesEntity(userId, productId)
+        when(val result = favoriteUseCases.insertFavoriteAndGetIdUseCase(favoriteEntity)) {
+            is Result.Error -> handleAddProductToFavoritesError(result.error)
+            is Result.Success -> setAddedToFavorites(itemIndex, true)
+        }
     }
 
     private fun handleAddProductToFavoritesError(error: DataError.Local) {
-        /*
-        when(error) {
-            DataError.Local.INSERTION_FAILED -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Ürün, favorilere eklenemedi."
-                )
-            }
-            DataError.Local.UNKNOWN -> {
-                _homeState.value = _homeState.value!!.copy(
-                    actionError = "Bilinmeyen bir hata meydana geldi."
-                )
-            }
-            else -> {}
+        val message = when(error) {
+            DataError.Local.INSERTION_FAILED -> "Ürün, favorilere eklenemedi."
+            DataError.Local.UNKNOWN -> "Bilinmeyen bir hata meydana geldi."
+            else -> null
+        }
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            actionError = message,
+            isLoading = false
+        )
+    }
+
+    fun removeProductFromFavoritesIfUserAuthenticated(productId: String, itemIndex: Int) {
+        val userId = _filteredProductsState.value!!.userId
+        if (userId == ANOMIM_USER_ID) {
+            _filteredProductsState.value = _filteredProductsState.value!!.copy(
+                shouldUserMoveToAuthActivity = true
+            )
+            return
         }
 
-         */
+        viewModelScope.launch {
+            checkIfProductNotInFavorites(userId, productId, itemIndex)
+        }
+    }
+
+    private suspend fun checkIfProductNotInFavorites(userId: String, productId: String, itemIndex: Int) {
+        when(val result = favoriteUseCases.checkIfProductInFavoritesUseCase(userId, productId)) {
+            is Result.Error -> handleCheckProductError(result.error)
+            is Result.Success -> {
+                if (result.data) {
+                    removeProductFromFavorites(userId, productId, itemIndex)
+                } else {
+                    setAddedToFavorites(itemIndex, false)
+                }
+            }
+        }
+    }
+
+    private suspend fun removeProductFromFavorites(userId: String, productId: String, itemIndex: Int) {
+        when(val result = favoriteUseCases.deleteFavoriteUseCase(userId, productId)) {
+            is Result.Error -> handleRemoveProductFromFavoritesError(result.error)
+            is Result.Success -> { setAddedToFavorites(itemIndex, false) }
+        }
+    }
+
+    private fun handleRemoveProductFromFavoritesError(error: DataError.Local) {
+        val message = when(error) {
+            DataError.Local.DELETION_FAILED -> "Ürün, favorilerden kaldırılamadı."
+            DataError.Local.UNKNOWN -> "Bilinmeyen bir hata meydana geldi."
+            else -> null
+        }
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            actionError = message,
+            isLoading = false
+        )
     }
 
     private fun setAddedToFavorites(itemIndex: Int, isAddedToFavorites: Boolean) {
-        /*
-        val currentProductList = _homeState.value!!.productList
+        val currentProductList = _filteredProductsState.value!!.productList
         currentProductList[itemIndex].addedToFavorites = isAddedToFavorites
-        _homeState.value = _homeState.value!!.copy(
+
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
             productList = currentProductList
         )
+    }
 
-         */
+
+    private fun handleCheckProductError(error: DataError.Local) {
+        val errorMessage = when(error) {
+            DataError.Local.NOT_FOUND -> { "İşleminiz gerçekleştirilemedi." }
+            DataError.Local.UNKNOWN -> { "Bilinmeyen bir hata nedeniyle ürünler getirilemedi." }
+            else -> null
+        }
+
+        _filteredProductsState.value = _filteredProductsState.value!!.copy(
+            actionError = errorMessage
+        )
     }
 }
